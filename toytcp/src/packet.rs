48516649bd3fdcc63628552205648746b415d16e
ac2393921 <ac2393921@gmail.com>
2025-04-29 22:49:58 +0900

    feat: TCP構造体にHeaderのgetter/setterとチェックサムの正誤判定ロジックを追加

--- toytcp/src/packet.rs
+++ toytcp/src/packet.rs
@@ -18,6 +18,51 @@ impl TCPPacket {
         }
     }
 
+    /// Get the source port
+    pub fn get_src(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[0], self.buffer[1]])
+    }
+
+    /// Get the destination port
+    pub fn get_dest(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[2], self.buffer[3]])
+    }
+
+    /// Get the sequence number
+    pub fn get_seq(&self) -> u32 {
+        u32::from_be_bytes([
+            self.buffer[4],
+            self.buffer[5],
+            self.buffer[6],
+            self.buffer[7],
+        ])
+    }
+
+    /// Get the acknowledgment number
+    pub fn get_ack(&self) -> u32 {
+        u32::from_be_bytes([
+            self.buffer[8],
+            self.buffer[9],
+            self.buffer[10],
+            self.buffer[11],
+        ])
+    }
+
+    /// Get the flag(SYN, ACK, PSH, RST, FIN)
+    pub fn get_flag(&self) -> u8 {
+        self.buffer[13]
+    }
+
+    /// Get the window size
+    pub fn get_window_size(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[14], self.buffer[15]])
+    }
+
+    /// Get the checksum
+    pub fn get_checksum(&self) -> u16 {
+        u16::from_be_bytes([self.buffer[16], self.buffer[17]])
+    }
+
     pub fn set_src(&mut self, port: u16) {
         self.buffer[0..2].copy_from_slice(&port.to_be_bytes());
     }
@@ -26,8 +71,44 @@ impl TCPPacket {
         self.buffer[2..4].copy_from_slice(&port.to_be_bytes());
     }
 
+    pub fn set_seq(&mut self, seq: u32) {
+        self.buffer[4..8].copy_from_slice(&seq.to_be_bytes());
+    }
+
+    pub fn set_ack(&mut self, ack: u32) {
+        self.buffer[8..12].copy_from_slice(&ack.to_be_bytes());
+    }
+
+    pub fn set_data_offset(&mut self, offset: u8) {
+        self.buffer[12] |= offset << 4;
+    }
+
     pub fn set_flag(&mut self, flag: u8) {
         self.buffer[13] = flag;
+    }
+
+    pub fn set_window_size(&mut self, size: u16) {
+        self.buffer[14..16].copy_from_slice(&size.to_be_bytes());
+    }
+
+    pub fn set_checksum(&mut self, checksum: u16) {
+        self.buffer[16..18].copy_from_slice(&checksum.to_be_bytes());
+    }
+
+    pub fn set_payload(&mut self, payload: &[u8]) {
+        self.buffer[TCP_HEADER_SIZE..TCP_HEADER_SIZE + payload.len()].copy_from_slice(payload);
+    }
+
+    pub fn is_correct_checksum(&self, local_addr: Ipv4Addr, remote_addr: Ipv4Addr) -> bool {
+        self.get_checksum()
+            == util::ipv4_checksum(
+                &self.buffer,
+                8,
+                &[],
+                &local_addr,
+                &remote_addr,
+                IpNextHeaderProtocols::Tcp,
+            )
     }
 }
 
